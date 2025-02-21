278e7a963b0f5de09be8243b30a5d32ee7f3133f
Proxtron <51761303+Proxtron@users.noreply.github.com>
2025-02-21 15:20:58 -0800

    Add logic for getting player choice

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ Get Random num from 1 to 3
 If num is 1 return rock
 If num is 2 return paper
 If num is 3 return scissors
+If invalid roll, return null
 */
 function getComputerChoice() {
     let randomRoll = Math.floor(Math.random() * 3) + 1;
@@ -14,8 +15,29 @@ function getComputerChoice() {
         case 3:
             return "Scissors";
         default:
-            return "";
+            return null;
     }
 }
 
-console.log(getComputerChoice());
+/*
+Prompt user with "Enter "1" for Rock, "2" for Paper, "3" for Scissors"
+Assign user input to userChoice variable
+If userChoice is 1, return Rock
+If userChoice is 2, return Paper
+If userChoice is 3, return Scissors
+If invalid input, return null
+*/
+
+function getHumanChoice() {
+    let userChoice = prompt(`Enter "1" for Rock, "2" for Paper, "3" for Scissors`);
+    switch(userChoice) {
+        case "1":
+            return "Rock";
+        case "2":
+            return "Paper";
+        case "3":
+            return "Scissors";
+        default:
+            return null;
+    }
+}

0985487bbbeaffb99313a19680dd708807b2629f
Proxtron <51761303+Proxtron@users.noreply.github.com>
2025-02-21 16:01:28 -0800

    Add logic for playing game of 5 rounds

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
-let humanScore = 0;
-let computerScore = 0;
+playGame();
 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-/*
-If human choice same as computerChoice, don't change the score
-If human choice is Rock
-    If computer choice is paper then increment computerScore
-    If computer choice is scissors then increment humanScore
-If human choice is Paper
-    If computer choice is scissors then increment computerScore
-    If computer choice is rock then increment humanScore
-If human choice is Scissors
-    If computer choice is rock then increment computerScore
-    If computer choice is paper then increment humanScore
-*/
-function playRound(humanChoice, computerChoice) {
-    console.log(`Human Choice: ${humanChoice}, Computer Choice: ${computerChoice}`);
-    if(humanChoice === "Rock") {
-        if(computerChoice === "Paper") {
+    for(let i = 1; i <= 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        console.log(`Round: ${i} Human Choice: ${humanChoice}, Computer Choice: ${computerChoice}`);
+
+        const roundWinner = playRound(humanChoice, computerChoice);
+        if(roundWinner === "Computer") {
             computerScore++;
-        } else if(computerChoice === "Scissors") {
+        } else if(roundWinner === "Human") {
             humanScore++;
+        }
+        console.log(`Round ${i} Human Score: ${humanScore}, Computer Score: ${computerScore}`);
+    }
+    
+    if(humanScore > computerScore) {
+        console.log("Human Wins!");
+    } else if (humanScore < computerScore) {
+        console.log("Computer Wins!");
+    } else {
+        console.log("Tie Game");
+    }
+}
+
+
+//Takes a human choice and a computer choice as arguments
+//Returns:
+//  "Human" if human wins the round
+//  "Computer" if computer wins the round
+//  "Tie"   if there is a tie
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === computerChoice) {
+        return "Tie";
+    } else if(humanChoice === "Rock") {
+        if(computerChoice === "Paper") {
+            return "Computer";
+        } else if(computerChoice === "Scissors") {
+            return "Human";
         }
     } else if(humanChoice === "Paper") {
         if(computerChoice === "Scissors") {
-            computerScore++;
+            return "Computer";
         } else if(computerChoice === "Rock") {
-            humanScore++;
+            return "Human";
         }
     } else if(humanChoice === "Scissors") {
         if(computerChoice === "Rock") {
-            computerScore++;
+            return "Computer";
         } else if(computerChoice === "Paper") {
-            humanScore++;
+            return "Human";
         }
     }
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
-console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
-/*
-Get Random num from 1 to 3
-If num is 1 return rock
-If num is 2 return paper
-If num is 3 return scissors
-If invalid roll, return null
-*/
+
+//console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
+
 function getComputerChoice() {
     let randomRoll = Math.floor(Math.random() * 3) + 1;
     switch(randomRoll) {
@@ -61,15 +73,6 @@ function getComputerChoice() {
             return null;
     }
 }
-
-/*
-Prompt user with "Enter "1" for Rock, "2" for Paper, "3" for Scissors"
-Assign user input to userChoice variable
-If userChoice is 1, return Rock
-If userChoice is 2, return Paper
-If userChoice is 3, return Scissors
-If invalid input, return null
-*/
 
 function getHumanChoice() {
     let userChoice = prompt(`Enter "1" for Rock, "2" for Paper, "3" for Scissors`);
